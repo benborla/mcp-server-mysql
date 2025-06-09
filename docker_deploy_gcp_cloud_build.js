b8b5e45c8c0e73d313f9d4cd85f2cfac4843be44
iofirag <iofirag@gmail.com>
2025-06-09 19:11:58 +0300

    chagnes

--- docker_deploy_gcp_cloud_build.js
+++ docker_deploy_gcp_cloud_build.js
@@ -6,11 +6,11 @@ const {
     ARTIFACT_REGISTRY_DOCKER_IMAGE_NAME,
 } = process.env;
 
-const image = `${ARTIFACT_REGISTRY_REPOSITORY}/${ARTIFACT_REGISTRY_DOCKER_IMAGE_NAME}_remote_build`;
+const image = `${ARTIFACT_REGISTRY_REPOSITORY}/${ARTIFACT_REGISTRY_DOCKER_IMAGE_NAME}_cloud_build`;
 
 const buildCmd = `gcloud builds submit --project=${GCLOUD_PROJECT_ID} --pack image=${image}`;
 console.log(buildCmd);
-let output = execSync(buildCmd + ' --verbosity=debug', { stdio: 'inherit' });
+let output = execSync(buildCmd, { stdio: 'inherit' });
 console.log(output);
 
 console.log("Done");
